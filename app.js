944aaf18163f1c5e299fc29eed91b61e7277e557
Tony Kalalian <tkalalian@gmail.com>
2023-06-05 14:15:51 +0300

    less redundancy in my code and better structure using Object orientation

--- app.js
+++ app.js
@@ -6,29 +6,19 @@ const authRoutes = require("./routes/auth");
 const request = require("request");
 const https = require("https");
 const { url } = require("inspector");
+
 const app = express();
 const port = process.env.PORT;
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use("/auth", authRoutes);
-// =============ALL MODELS FOR MUSCLES==============
-const ChestPost = require("./models/ChestPost");
-const AbsPost = require("./models/AbsPost");
-const AdductorsPost = require("./models/AdductorsPost");
-const BicepsPost = require("./models/BicepsPost");
-const TrapeziusPost = require("./models/TrapeziusPost");
-const ForearmPost = require("./models/ForearmPost");
-const CalvesPost = require("./models/CalvesPost");
-const DeltoidsPost = require("./models/DeltoidsPost");
-const LatsPost = require("./models/LatsPost");
-const HamstringPost = require("./models/HamstringPost");
-const QuadsPost = require("./models/QuadsPost");
-const TricepsPost = require("./models/TricepsPost");
-const ObliquesPost = require("./models/ObliquesPost");
-const GlutesPost = require("./models/GlutesPost");
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -37,6 +27,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
 
+    // Helper function to save posts to the database
     const savePostsToDB = async (model, posts) => {
       try {
         await model.deleteMany({});
@@ -49,165 +40,81 @@ mongoose
       }
     };
 
+    // Routes
     app.get("/home", (req, res) => {
       res.render("home");
     });
+
     app.get("/", (req, res) => {
       res.render("index");
     });
+
     app.get("/login", (req, res) => {
       res.render("login");
     });
+
     app.get("/bmr", (req, res) => {
       res.render("bmr");
     });
+
     app.get("/registration", (req, res) => {
       res.render("registration");
     });
+
     app.get("/plans", (req, res) => {
       res.render("plans");
     });
-    app.get("/chest", async (req, res) => {
-      try {
-        const posts = await ChestPost.find();
-        res.render("chest", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
+
+    // Dynamic routes for different muscle groups
+    const muscleRoutes = [
+      { path: "/chest", model: ChestPost },
+      { path: "/abs", model: AbsPost },
+      { path: "/adductors", model: AdductorsPost },
+      { path: "/biceps", model: BicepsPost },
+      { path: "/glutes", model: GlutesPost },
+      { path: "/trapezius", model: TrapeziusPost },
+      { path: "/forearm", model: ForearmPost },
+      { path: "/calves", model: CalvesPost },
+      { path: "/triceps", model: TricepsPost },
+      { path: "/deltoids", model: DeltoidsPost },
+      { path: "/quads", model: QuadsPost },
+      { path: "/obliques", model: ObliquesPost },
+      { path: "/lats", model: LatsPost },
+      { path: "/hamstring", model: HamstringPost },
+    ];
+
+    muscleRoutes.forEach((route) => {
+      app.get(route.path, async (req, res) => {
+        try {
+          const posts = await route.model.find();
+          res.render(route.path.substr(1), { posts });
+        } catch (error) {
+          console.log(error);
+          res.status(500).json({ error: "An error occurred" });
+        }
+      });
     });
-    app.get("/abs", async (req, res) => {
-      try {
-        const posts = await AbsPost.find();
-        res.render("abs", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/adductors", async (req, res) => {
-      try {
-        const posts = await AdductorsPost.find();
-        res.render("adductors", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/biceps", async (req, res) => {
-      try {
-        const posts = await BicepsPost.find();
-        res.render("biceps", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/glutes", async (req, res) => {
-      try {
-        const posts = await GlutesPost.find();
-        res.render("glutes", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/trapezius", async (req, res) => {
-      try {
-        const posts = await TrapeziusPost.find();
-        res.render("trapezius", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/forearm", async (req, res) => {
-      try {
-        const posts = await ForearmPost.find();
-        res.render("forearm", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/calves", async (req, res) => {
-      try {
-        const posts = await CalvesPost.find();
-        res.render("calves", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/triceps", async (req, res) => {
-      try {
-        const posts = await TricepsPost.find();
-        res.render("triceps", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/deltoids", async (req, res) => {
-      try {
-        const posts = await DeltoidsPost.find();
-        res.render("deltoids", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/quads", async (req, res) => {
-      try {
-        const posts = await QuadsPost.find();
-        res.render("quads", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/obliques", async (req, res) => {
-      try {
-        const posts = await ObliquesPost.find();
-        res.render("obliques", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/lats", async (req, res) => {
-      try {
-        const posts = await LatsPost.find();
-        res.render("lats", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
-    app.get("/hamstring", async (req, res) => {
-      try {
-        const posts = await HamstringPost.find();
-        res.render("hamstring", { posts });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      }
-    });
+
     app.get("/food", (req, res) => {
       res.render("food");
     });
+
     app.get("/diet", (req, res) => {
       res.render("diet");
     });
+
     app.get("/product", (req, res) => {
       res.render("product");
     });
+
     app.get("/checkout", (req, res) => {
       res.render("checkout");
     });
+
     app.get("/team", (req, res) => {
       res.render("team");
     });
+
     app.post("/news", (req, res) => {
       const fname = req.body.fname;
       const lname = req.body.lname;
@@ -245,9 +152,11 @@ mongoose
       request.write(jsonData);
       request.end();
     });
+
     app.post("/failure", (req, res) => {
       res.redirect("/news");
     });
+
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
@@ -255,3 +164,19 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB", error);
   });
+
+// Models for muscle posts
+const ChestPost = require("./models/ChestPost");
+const AbsPost = require("./models/AbsPost");
+const AdductorsPost = require("./models/AdductorsPost");
+const BicepsPost = require("./models/BicepsPost");
+const TrapeziusPost = require("./models/TrapeziusPost");
+const ForearmPost = require("./models/ForearmPost");
+const CalvesPost = require("./models/CalvesPost");
+const DeltoidsPost = require("./models/DeltoidsPost");
+const LatsPost = require("./models/LatsPost");
+const HamstringPost = require("./models/HamstringPost");
+const QuadsPost = require("./models/QuadsPost");
+const TricepsPost = require("./models/TricepsPost");
+const ObliquesPost = require("./models/ObliquesPost");
+const GlutesPost = require("./models/GlutesPost");
